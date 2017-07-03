08486679218ef0e136691f091eba08d41de81e33
Saman Saadi <QSaman@users.noreply.github.com>
2017-07-02 23:34:30 -0400

    Solve shaders exercise 1. Change the structure of HelloTriangle exercises.

---
+++ Chapters/Shaders/ShadersExercise01/src/main.cpp
@@ -0,0 +1,149 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cmath>
+
+#include "shader.h"
+
+#define WIDTH 800
+#define HEIGHT 600
+
+const bool enableWireframeMode = false;
+
+GLFWwindow* initGLFW()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //Fixing compilation on OS X
+#endif
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Hello OpenGL", NULL, NULL);
+    if (window != NULL)
+        glfwMakeContextCurrent(window);
+    return window;
+}
+
+bool loadOpenGL()
+{
+    return gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+}
+
+//Callbacks:
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void setGLFWcallbacks(GLFWwindow* window)
+{
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void setupVAO(GLuint& vao, GLuint& vbo)
+{
+    struct Vertex
+    {
+        GLfloat position[3];
+        GLfloat color[3];
+    };
+
+
+    Vertex vertices[3] =
+	{
+        {{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}},     //left
+        {{0.5f, -0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}},      //right
+        {{0.0f, 0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}}		//top
+	};
+
+	glGenVertexArrays(1, &vao);
+
+	glGenBuffers(1, &vbo);
+
+	glBindVertexArray(vao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), (void*)NULL);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, false, sizeof(Vertex), (void*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered 
+	//VBO as the vertex attribute's bound vertex buffer object so afterwards 
+	//we can safely unbind:
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally 
+	//modify this VAO, but this rarely happens. Modifying other VAOs requires 
+	//a call to glBindVertexArray anyways so we generally don't unbind VAOs 
+	//(nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+}
+
+void renderFrame(Shader& shader, GLuint vao)
+{
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    shader.use();
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+int main()
+{
+    auto window = initGLFW();
+	if (window == NULL)
+	{
+        std::cerr << "Cannot create a GLFW window" << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+
+    setGLFWcallbacks(window);
+
+    if (!loadOpenGL())
+    {
+        std::cout << "Failed to load OpenGL" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    Shader shader("shaders/shader.vs", "shaders/shader.fs");
+    try
+    {
+        shader.loadShaders();
+    }
+    catch (std::string str)
+    {
+        std::cerr << str << std::endl;
+        return 1;
+    }
+
+	GLuint vao, vbo;
+	setupVAO(vao, vbo);
+
+	if (enableWireframeMode)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+        renderFrame(shader, vao);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
+    glfwTerminate();
+}
---
+++ Chapters/Shaders/ShadersExercise01/src/shader.h
@@ -0,0 +1,114 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <memory>
+#include <iostream>
+#include <exception>
+
+class Shader
+{
+public:
+    explicit Shader(const char* vertexPath, const char* fragmentPath) :
+        vertexPath{vertexPath}, fragmentPath{fragmentPath}
+    {
+    }
+
+    void loadShaders()
+    {
+        std::string vertexCode = loadShader(vertexPath);
+        std::string fragmentCode = loadShader(fragmentPath);
+
+        auto vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+        const char* vertexCodeC = vertexCode.c_str();
+        glShaderSource(vertexShaderId, 1, &vertexCodeC, NULL);
+        if (!compileShader(vertexShaderId))
+        {
+            std::string message = getCompileErrorMessage(vertexShaderId);
+            throw("Vertex Shader Compilation Error: " + message);
+        }
+
+        auto fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+        const char* fragmentCodeC = fragmentCode.c_str();
+        glShaderSource(fragmentShaderId, 1, &fragmentCodeC, NULL);
+        if (!compileShader(fragmentShaderId))
+        {
+            std::string message = getCompileErrorMessage(fragmentShaderId);
+            throw("Fragment Shader Compilation Error: " + message);
+        }
+
+        programId = glCreateProgram();
+        glAttachShader(programId, vertexShaderId);
+        glAttachShader(programId, fragmentShaderId);
+        if (!linkShaders())
+            throw ("Linking shaders error: " + getLinkErrorMessage());
+        glDeleteShader(vertexShaderId);
+        glDeleteShader(fragmentShaderId);
+    }
+
+    void use()
+    {
+        glUseProgram(programId);
+    }
+
+private:
+    std::string loadShader(const char* shaderPath)
+    {
+        std::ifstream shaderFile;
+
+        shaderFile.exceptions(std::ifstream::badbit | std::ifstream::failbit);
+        shaderFile.open(shaderPath);
+
+        std::ostringstream os;
+        os << shaderFile.rdbuf();
+        return os.str();
+    }
+
+    GLint compileShader(GLuint shader)
+    {
+        glCompileShader(shader);
+        GLint success;
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        return success;
+    }
+
+    std::string getCompileErrorMessage(GLuint shader)
+    {
+        GLint size;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &size);
+        if (size == 0)
+            return "";
+        std::unique_ptr<GLchar[]> message(new GLchar[size]);
+        glGetShaderInfoLog(shader, size, NULL, message.get());
+        return std::string(message.get());
+    }
+
+    GLint linkShaders()
+    {
+        glLinkProgram(programId);
+        GLint success;
+        glGetProgramiv(programId, GL_LINK_STATUS, &success);
+        return success;
+    }
+
+    std::string getLinkErrorMessage()
+    {
+        GLint size;
+        glGetProgramiv(programId, GL_INFO_LOG_LENGTH, &size);
+        if (size == 0)
+            return "";
+        std::unique_ptr<GLchar[]> message(new GLchar[size]);
+        glGetProgramInfoLog(programId, size, NULL, message.get());
+        return std::string(message.get());
+    }
+
+private:
+    GLuint programId;
+    const char* vertexPath, * fragmentPath;
+};
+
+#endif
