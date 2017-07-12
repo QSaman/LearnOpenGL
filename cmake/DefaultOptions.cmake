a9bf288c53ebd7918af19adf21326330aff4cd4d
Saman Saadi <QSaman@users.noreply.github.com>
2017-07-11 23:49:18 -0400

    Use DefaultOptions script for all projects.

--- cmake/DefaultOptions.cmake
+++ cmake/DefaultOptions.cmake
@@ -1,7 +1,9 @@
 # Allow subfolders in solution file
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 
+
 if (CMAKE_VERSION VERSION_LESS "3.1")
+        message(WARNING "It is recommended to use at least CMake 3.1")
 	set(DEFAULT_TARGET_OPTIONS
 		CXX_STANDARD 11
 		CXX_STANDARD_REQUIRED ON
