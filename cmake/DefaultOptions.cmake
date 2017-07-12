63b1a6c8cd54493376cf1295a1025759c378a493
Saman Saadi <QSaman@users.noreply.github.com>
2017-07-12 01:39:41 -0400

    Correct CMake script versions.

--- cmake/DefaultOptions.cmake
+++ cmake/DefaultOptions.cmake
@@ -1,9 +1,11 @@
+cmake_minimum_required(VERSION 3.1)
+
 # Allow subfolders in solution file
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 
 
-if (CMAKE_VERSION VERSION_LESS "3.1")
-        message(WARNING "It is recommended to use at least CMake 3.1")
+#CXX_STANDARD requires CMake 3.1
+if (CXX_STANDARD_PER_TARGET)
 	set(DEFAULT_TARGET_OPTIONS
 		CXX_STANDARD 11
 		CXX_STANDARD_REQUIRED ON
